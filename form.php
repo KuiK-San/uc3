b8aafbe72a886502b00fc89cb73c20ef6b3f9393
Guilherme Casagrande <thegui4000@gmail.com>
2022-07-28 16:16:43 -0300

    sda

---
+++ form.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form PHP</title>
+</head>
+<body>
+    <form action="" method="post">
+        <p>
+            <label for="" name="titulo">Titulo</label>
+            <input type="text" name="titulo" id="">
+        </p>
+        <p>
+            <label for="">Descrição</label>
+            <textarea name="descricao" cols="30" rows="10"></textarea>
+        </p>
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
