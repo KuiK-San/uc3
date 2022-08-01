12eb9ea96d97927f0d8882299fe7271425af8287
Guilherme Casagrande <thegui4000@gmail.com>
2022-08-01 14:26:46 -0300

    nhenhenhe

--- form.php
+++ form.php
@@ -1,23 +1,29 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form PHP</title>
 </head>
+
 <body>
     <h2>CRUD</h2>
     <form action="processa.php" method="post">
-        <p>
-            <label for="" name="titulo">Titulo</label> <br>
-            <input type="text" name="titulo" id="">
-        </p>
-        <p>
-            <label for="">Descrição</label> <br>
-            <textarea name="descricao" cols="30" rows="10"></textarea>
-        </p>
-        <input type="submit" value="Enviar">
+        <fieldset>
+            <legend>Cadastro de tarefas</legend>
+            <p>
+                <label for="" name="titulo">Titulo</label> <br>
+                <input type="text" name="titulo" id="">
+            </p>
+            <p>
+                <label for="">Descrição</label> <br>
+                <textarea name="descricao" cols="30" rows="10"></textarea>
+            </p>
+            <input type="submit" value="Enviar">
+        </fieldset>
     </form>
 </body>
+
 </html>
